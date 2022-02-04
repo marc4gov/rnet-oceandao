5edaf1403cfee7a15179b823e937c4ffe7d718d6
Marc Minnee <marc4gov@users.noreply.github.com>
2022-02-04 12:45:44 +0100

    update sim_setup

---
+++ model/sim_setup.py
@@ -0,0 +1,8 @@
+print("running file: sim_setup.py")
+# T
+SIMULATION_TIME_STEPS = 360
+print("SIMULATION_TIME_STEPS: ", SIMULATION_TIME_STEPS)
+# N monte carlo runs
+MONTE_CARLO_RUNS = 3
+print("MONTE_CARLO_RUNS: ", MONTE_CARLO_RUNS)
+print("end of file: sim_setup.py")
